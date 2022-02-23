62df0cbf8a382e444930fbe0fb7585040a8f9658
rikrikrahadian <rikrik.rahadian@gmail.com>
2022-02-23 23:50:33 +0700

    percobaan

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,7 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        print("oke")
 
 }
 
@@ -12,4 +13,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        print("oke")
 }
